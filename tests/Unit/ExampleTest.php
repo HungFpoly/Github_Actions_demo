3c2b16511f40e306075f38a24cb4eb26b2fb6943
unknown <you@example.com>
2021-11-04 11:40:31 +0700

    up

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,6 +15,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
     }
 }
